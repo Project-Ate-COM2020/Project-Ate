378b705adb47813bbd83cc128455bf285c8816ff
a-someone-person <17clarkeja@gmail.com>
2026-01-17 22:08:45 +0000

    Setting up Django-rest-framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # For specific API development
     'rest_framework',
 ]
 
