8936927ea3656529388a0e9c215d34a41d6dc558
jacob evans <jacobevans4@proton.me>
2026-01-20 18:27:51 +0000

    add django model for the Bundle database entity

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
-# Create your models here.
 class Seller(models.Model):
     name = models.CharField(max_length=20)
     # just do post code for now
     location = models.CharField(max_length=6)
     opening_hours = models.CharField(max_length=20)
     contact_stub = models.CharField(max_length=20)
+
+
+class Bundle(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    category = models.CharField(max_length=20)
+    contents = models.TextField()
+    allergens = models.TextField()
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    pickup_window = models.CharField(max_length=20)
+    status = models.IntegerField()
