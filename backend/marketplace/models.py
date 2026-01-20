fc545a9830b27108be76ad34002800cd69c585cd
jacob evans <jacobevans4@proton.me>
2026-01-20 18:24:12 +0000

    create django mode for the seller database entity

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Seller(models.Model):
+    name = models.CharField(max_length=20)
+    # just do post code for now
+    location = models.CharField(max_length=6)
+    opening_hours = models.CharField(max_length=20)
+    contact_stub = models.CharField(max_length=20)
