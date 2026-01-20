f0e73d559825228b91f2733e64925ac6cc531b33
jacob evans <jacobevans4@proton.me>
2026-01-20 21:34:25 +0000

    add bundle view to get info about a specific bundle, change bundles view to return all bundle ids. This will be more performant as data grows.

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("marketplace/", views.Bundles.as_view(), name="marketplace"),
+    path("marketplace/bundles", views.BundlesView.as_view(), name="bundles"),
+    path(
+        "marketplace/bundle/<int:bundle_id>/", views.BundleView.as_view(), name="bundle"
+    ),
 ]
