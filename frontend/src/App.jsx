6113910e9ca3749e1d07d5f95660859d8f53f398
a-someone-person <17clarkeja@gmail.com>
2026-01-20 16:32:46 +0000

    Added extra folders for separated frontend dev, and slightly changed App.jsx - we won't need the file at all much longer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,6 @@ import axios from 'axios';
 function App() {
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/items/')
-      .then(res => setItems(res.data))
-      .catch(err => console.error(err));
-  }, []);
 
   return (
     <div>
