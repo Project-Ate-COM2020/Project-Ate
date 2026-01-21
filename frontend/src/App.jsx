4e9b577748a7894708b2170db7540e689e5e3493
JAlexanderC <17clarkeja@gmail.com>
2026-01-21 15:20:44 +0000

    started with a skeleton router for react frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,23 @@
-// This File is where we will route webpages - We will define what URL to serve each react component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AnalyticsPage from "./analyticsPages/analytics";
+import GamePage from "./gamePages/game";
+import MarketplacePage from "./marketplacePages/marketplace";
+import NotFoundPage from "./reusableComponents/NotFoundPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AnalyticsPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/marketplace" element={<MarketplacePage />} />
+        <Route path="/analytics" element={<analyticsPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* This is the pgae we show if URL doesn't exist */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
