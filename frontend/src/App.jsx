62343346159493bc49326dbbaa3316dace0c64a2
JAlexanderC <17clarkeja@gmail.com>
2026-01-21 15:23:05 +0000

    Added readable comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,26 @@
+// import react and react router
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// import the files for each URL endpoint
 import AnalyticsPage from "./analyticsPages/analytics";
 import GamePage from "./gamePages/game";
 import MarketplacePage from "./marketplacePages/marketplace";
 import NotFoundPage from "./reusableComponents/NotFoundPage";
 
+// simple function defining the element to be returned based on the URL
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AnalyticsPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/marketplace" element={<MarketplacePage />} />
-        <Route path="/analytics" element={<analyticsPage />} />
-        <Route path="*" element={<NotFoundPage />} /> {/* This is the pgae we show if URL doesn't exist */}
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
 }
 
+// export the app
 export default App;
