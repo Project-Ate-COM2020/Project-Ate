6b8cb2543d080e4db3a038bf6ed13c41cabfd145
zacdowland <zac.dowland2006@icloud.com>
2026-01-26 19:52:12 +0000

    made a test view that doesnt work, problem with django i believe

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GameSummaryView, RecentRescuesView
+from .views import GameSummaryView, RecentRescuesView, TestView
 
 urlpatterns = [
     path("api/game/summary/", GameSummaryView.as_view(), name="game-summary"),
-    path("api/game/recent/", RecentRescuesView.as_view(), name="game-recent")
+    path("api/game/recent/", RecentRescuesView.as_view(), name="game-recent"),
+    path("api/test/", TestView.as_view())
 ]
