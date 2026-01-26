f82bf8ba12747067c2fb61a7500e431847f1f0be
Lucas Parish <lucasparish84@gmail.com>
2026-01-26 22:52:17 +0000

    implemented db partially

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    
     
     # For specific API development
     'rest_framework',
