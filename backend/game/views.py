db2bb6cd09c8c220ef1a0f41b7a4cd57529a2a2e
zacdowland <zac.dowland2006@icloud.com>
2026-01-26 17:54:16 +0000

    created the base of the code for game backend, just need to test

--- backend/game/views.py
+++ backend/game/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import isAuthenticated
+from rest_framework.generics import ListAPIView
+from .models import Consumer, Reservation
+from .serializers import ReservationSerializer
+
 # Create your views here.
+
+class GameSummaryView(APIView):
+    permission_classes = isAuthenticated
+
+    def get(self, request):
+
+        consumer = request.user.consumer
+
+        collected = Reservation.objects.filter(consumer=consumer, status="collected")
+        
+        return Response({
+            "current_streak": consumer.streak, 
+            "total_rescues": collected.count(), 
+            "co2_estimate": collected.count * 2
+            ##using 2 as a guess for average co2 used in a meal
+            })
+    
+class RecentRescuesView(ListAPIView):
+    permission_classes = isAuthenticated
+    serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        consumer = self.request.user.consumer
+        limit = int(self.request.query_params.get("limit", 10))
+
+        return (
+            Reservation.objects.filter(consumer=consumer, status="collected").order_by("collected_at")[:limit]
+        )
