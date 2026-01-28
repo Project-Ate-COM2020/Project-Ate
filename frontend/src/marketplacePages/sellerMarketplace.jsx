274640f11a8f52536218e8be6f0b920188300921
Jamie <17clarkeja@gmail.com>
2026-01-28 17:45:19 +0000

    Initial marketplace template

---
+++ frontend/src/marketplacePages/sellerMarketplace.jsx
@@ -0,0 +1,12 @@
+import { React } from  "react"
+
+function SellerMarketplace(){
+    return(
+        <div>
+            <h1>Seller Marketplace Page</h1>
+            <p>Welcome to the Seller Marketplace! Here you can manage your listings and view your sales performance.</p>
+        </div>
+    );
+}
+
+export default SellerMarketplace;
