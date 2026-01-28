f7bb539c5b5b09cf67113795a842a3fc99104963
Jamie <17clarkeja@gmail.com>
2026-01-28 17:47:11 +0000

    added navbar (also did this for analytics)

--- frontend/src/marketplacePages/sellerMarketplace.jsx
+++ frontend/src/marketplacePages/sellerMarketplace.jsx
@@ -1,8 +1,10 @@
 import { React } from  "react"
+import NavBar from "../reusableComponents/navBar.jsx";
 
 function SellerMarketplace(){
     return(
         <div>
+            <NavBar />
             <h1>Seller Marketplace Page</h1>
             <p>Welcome to the Seller Marketplace! Here you can manage your listings and view your sales performance.</p>
         </div>
