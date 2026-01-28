ce15962ceb073060c7a948f800c6dca2365541aa
Jamie <17clarkeja@gmail.com>
2026-01-28 08:58:38 +0000

    base analytics

--- frontend/src/analyticsPages/analytics.jsx
+++ frontend/src/analyticsPages/analytics.jsx
@@ -2,7 +2,22 @@ import React from "react";
 
 function Analytics() {
     return (
+        <div>
         <title>Seller Analytics</title>
+        <h1>Analytics</h1>
+        <hr />
+        <h2>Sales Data:</h2>
+        <p>GET SALES DATA</p>
+        <hr />
+        <h2>Percentage No shows:</h2>
+        <p>GET NO SHOW DATA</p>
+        <hr />
+        <h2>Total revenue</h2>
+        <p>GET REVENUE DATA</p>
+        <hr />
+        <h2>Repeat for other metrics we track</h2>
+        <p>GET DATA</p>
+        </div>
     )
 }
 
