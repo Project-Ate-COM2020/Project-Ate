d5840db28e017a693038dd58bc9f46286db264e9
jacob evans <jacobevans4@proton.me>
2026-01-29 13:55:55 +0000

    [WIP] add creating consumers, sellers, reservations through post req and outline endpoints

---
+++ backend/marketplace/views/reservation.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from ..models import ReservationSerializer, Reservation
+
+
+class CreateReservationView(CreateAPIView):
+    name = "reservations-create"
+    serializer_class = ReservationSerializer
+    queryset = Reservation
---
+++ backend/marketplace/views/seller.py
@@ -0,0 +1,78 @@
+from rest_framework import status
+from rest_framework.settings import APISettings
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.response import Response
+
+from ..models import Seller, SellerSerializer, BundleSerializer
+from ..models import Bundle
+
+
+class CreateSellerView(CreateAPIView):
+    name = "seller-create"
+    queryset = Seller
+    serializer_class = SellerSerializer
+
+
+class SellerView(RetrieveUpdateDestroyAPIView):
+    name = "seller"
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+
+
+# get all bundles by a seller
+class SellerBundlesView(APIView):
+    name: str = "seller-bundles"
+
+    def get(self, request, seller_id):
+        try:
+            seller = Seller.objects.get(pk=seller_id)
+
+            bundle = Bundle.objects.get(seller=seller)
+
+            serializer = BundleSerializer(bundle)
+
+            return Response(serializer.data)
+        except Bundle.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# get newest bundles
+class SellerBundleNewestView(APIView):
+    name = "seller-bundles-newest"
+
+    def get(self, request, seller_id):
+        count = request.GET.get("count", 20)
+
+
+# get oldest bundles
+class SellerBundleOldestView(APIView):
+    name = "seller-bundles-oldest"
+
+    def get(self, request, seller_id):
+        count = request.GET.get("count", 20)
+
+
+# get bundles made between date range bundles
+class SellerBundleBetweenView(APIView):
+    name = "seller-bundles-between"
+
+    def get(self, request, seller_id):
+        date_from = request.GET.get("from")
+        date_to = request.GET.get("to")
+
+
+# get bundles made between date range bundles
+class SellerBundleOlderView(APIView):
+    name = "seller-bundles-older"
+
+    def get(self, request, seller_id):
+        date = request.GET.get("date")
+
+
+# get bundles newer than a specified date
+class SellerBundleNewerView(APIView):
+    name = "seller-bundles-newer"
+
+    def get(self, request, seller_id):
+        date = request.GET.get("date")
