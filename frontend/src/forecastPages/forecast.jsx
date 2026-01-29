53a165e8f7d748094f1ce9b7b1bd5f607cc795ea
Jamie <17clarkeja@gmail.com>
2026-01-29 15:58:12 +0000

    simple api getter based on my analytics one - I need to make API fetching and data retrieval consistent across the frontend with will and harry TODO

--- frontend/src/forecastPages/forecast.jsx
+++ frontend/src/forecastPages/forecast.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import NavBar from "../reusableComponents/navBar.jsx";
 
 function ForecastPage(){
-    return(
-    <div>
-    <div>
-        <NavBar />
-    </div>
-    <div>
-        <title>Sales Forecast Page</title>
-        <h1>Sales Forecasts for: GET SELLER NAME</h1>
-        <hr />
-        <h2>Forecast Overview:</h2>
-        <p>GET FORECAST DATA</p>
-        <hr />
-    </div>
-    </div>
-    );
+    const [forecasts, setForecasts] = useState([]);
 
+
+    // get API data using api/.js helpers
+    const loadData = async () => {
+        const [data, setData] = useState(null);
+        const [loading, setLoading] = useState(true);
+        useEffect(() => {
+            const loadData = async () => {
+                setLoading(true);
+                const response = await fetch("/forecasts"); // need to connect to json to get all forecast data
+                const data = await response.json();
+                setForecasts(data);
+                setLoading(false);
+            }
+        }, []);
+    }
+    
+    // now forecasts will contain the fetched data from /forecasts endpoint THIS IS NOT NEARLY FINISHED ONLY FOR OWN DEV
+
+    return(
+        <div>
+            <div>
+                <NavBar />
+            </div>
+            <div>
+                <title>Sales Forecast Page</title>
+                <h1>Sales Forecasts for: GET SELLER NAME</h1>
+                <hr />
+                
+                <div>
+                </div>
+                <hr />
+            </div>
+        </div>
+    );
 }
 
 export default ForecastPage;
