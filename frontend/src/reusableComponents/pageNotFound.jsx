0de8c252720e7c02a69fb73e4d6e1f5bf5bb2fa3
James Clarke <17clarkeja@gmail.com>
2026-01-30 12:58:36 +0000

    added root colour scheme, starting to flesh out

--- frontend/src/reusableComponents/pageNotFound.jsx
+++ frontend/src/reusableComponents/pageNotFound.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import NavBar from "./navBar";
 
 function Pagenotfound() {
     return (
+        <div>
+        <NavBar />
         <p>Page not found</p>
+        </div>
     )
 }
 
