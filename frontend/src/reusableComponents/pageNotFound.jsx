f00c9b75533cb0dc65643f4642d155c0fe66ff8f
Jamie <17clarkeja@gmail.com>
2026-02-01 11:16:57 +0000

    made pageNotFound CSS

--- frontend/src/reusableComponents/pageNotFound.jsx
+++ frontend/src/reusableComponents/pageNotFound.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "./navBar";
 import "./root.css";
+import "./pageNotFound.css";
 
 function Pagenotfound() {
     return (
         <div>
         <NavBar />
-        <p>Page not found</p>
+        <p>Page not found ):</p>
         </div>
     )
 }
