7040bdfd6b4fcc90d7393e8f0860951a5ecca3f6
zacdowland <zac.dowland2006@icloud.com>
2026-02-02 17:36:16 +0000

    added the bundle_posting model and updated the reservation model

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -4,4 +4,13 @@ from .models import Reservation
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
-        fields = ["reservation_id", "posting_id", "status", "collected_at"]
+        fields = [
+            "reservation_id", 
+            "posting_id", 
+            "consumer_id",
+            "timestamp",
+            "claim_code", 
+            "status", 
+            "no_show_reason",
+            "collected_at"
+        ]
