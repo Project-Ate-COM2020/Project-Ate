b00bcef463728dea76a4e883c9e9a967562ab277
zacdowland <zac.dowland2006@icloud.com>
2026-02-02 18:12:51 +0000

    updated reservation model and implemented the co2 estimate code

--- backend/game/models.py
+++ backend/game/models.py
@@ -10,22 +10,6 @@ class Consumer(models.Model):
     badges = models.JSONField(default=list)
     # making badges a list so names of the badges can be put in there
 
-# defining the reservation table to django
-class Reservation(models.Model):
-    reservation_id = models.AutoField(primary_key=True)
-    posting_id = models.IntegerField()
-    # keeping posting_id as just a number for now, can be changed to a foreign key later
-    consumer_id = models.ForeignKey(Consumer, on_delete=models.CASCADE)
-    # establishing consumer_id as a foreign key so we can do the recent reservations count
-    # if a consumer is deleted then reservations are also deleted 
-    timestamp = models.DateTimeField()
-    claim_code = models.CharField(max_length=20)
-    STATUS_CHOICES = [("no-show", "No show"), ("collected", "Collected"), ("expired", "Expired")]
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    # defines status as only being able to take the values no-show, collected or expired
-    no_show_reason = models.CharField(max_length=50)
-    collected_at = models.DateTimeField() 
-
 # defining the bundle_posting table to django
 class Bundle_Posting(models.Model):
     posting_id = models.AutoField(primary_key=True)
@@ -49,4 +33,22 @@ class Bundle_Posting(models.Model):
     pickup_window = models.CharField(max_length=20)
     status = models.CharField(max_length=20)
     created_at = models.DateTimeField()
-    updated_at = models.DateTimeField() 
+    updated_at = models.DateTimeField()
+     
+# defining the reservation table to django
+class Reservation(models.Model):
+    reservation_id = models.AutoField(primary_key=True)
+    posting_id = models.ForeignKey(Bundle_Posting, on_delete=models.CASCADE)
+    # establishing posting_id as a foreign key so we can do the co2 estimate
+    # if a bundle posting is deleted then reservations are also deleted
+    consumer_id = models.ForeignKey(Consumer, on_delete=models.CASCADE)
+    # establishing consumer_id as a foreign key so we can do the recent reservations count
+    # if a consumer is deleted then reservations are also deleted 
+    timestamp = models.DateTimeField()
+    claim_code = models.CharField(max_length=20)
+    STATUS_CHOICES = [("no-show", "No show"), ("collected", "Collected"), ("expired", "Expired")]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    # defines status as only being able to take the values no-show, collected or expired
+    no_show_reason = models.CharField(max_length=50)
+    collected_at = models.DateTimeField() 
+
