6498440b8794a466e4ef4810dff028c43f04bc39
zacdowland <zac.dowland2006@icloud.com>
2026-02-02 16:26:44 +0000

    added comments

--- backend/game/models.py
+++ backend/game/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-##defining the consumer table to django
+# defining the consumer table to django
 class Consumer(models.Model):
     consumer_id = models.AutoField(primary_key=True)
     display_name = models.CharField(max_length=50)
     streak = models.IntegerField(default=0)
     badges = models.JSONField(default=list)
-    ##making badges a list so names of the badges can be put in there
+    # making badges a list so names of the badges can be put in there
 
-##defining the reservation table to django
+# defining the reservation table to django
 class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
     posting_id = models.IntegerField()
-    ##keeping posting_id as just a number for now, can be changed to a foreign key later
+    # keeping posting_id as just a number for now, can be changed to a foreign key later
     consumer_id = models.ForeignKey(Consumer, on_delete=models.CASCADE)
-    ##establishing consumer_id as a foreign key so we can do the recent reservations count
-    ##if a consumer is deleted then reservations are also deleted 
+    # establishing consumer_id as a foreign key so we can do the recent reservations count
+    # if a consumer is deleted then reservations are also deleted 
     STATUS_CHOICES = [("no-show", "No show"), ("collected", "Collected"), ("expired", "Expired")]
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    ##defines status as only being able to take the values no-show, collected or expired
+    # defines status as only being able to take the values no-show, collected or expired
     collected_at = models.DateTimeField() 
 
     
--- backend/game/views.py
+++ backend/game/views.py
@@ -18,12 +18,15 @@ class GameSummaryView(APIView):
         consumer = request.user.consumer
 
         collected = Reservation.objects.filter(consumer=consumer, status="collected")
+        # all the records that have been collected by the consumer
         
         return Response({
             "current_streak": consumer.streak, 
             "total_rescues": collected.count(), 
             "co2_estimate": collected.count * 2
             # using 2 as a guess for average co2 used in a meal
+            # may need to go more in depth here, e.g. working out different categories of food have different co2 values
+            # using the quantity from the 'bundle_posting' entity
             })
     
 class RecentRescuesView(ListAPIView):
@@ -33,9 +36,11 @@ class RecentRescuesView(ListAPIView):
     def get_queryset(self):
         consumer = self.request.user.consumer
         limit = int(self.request.query_params.get("limit", 10))
+        # sets the limit at 10 so only the last 10 records are shown
 
         return (
             Reservation.objects.filter(consumer=consumer, status="collected").order_by("collected_at")[:limit]
+            # gets all the records that are "collected" and the limit is set
         )
 
 # test view
