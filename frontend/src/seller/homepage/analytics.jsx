0ed2c4462147209739b32457343e32ae35ea67c5
Jamie <17clarkeja@gmail.com>
2026-02-03 00:01:03 +0000

    should have finished new analytics frontend

--- frontend/src/seller/homepage/analytics.jsx
+++ frontend/src/seller/homepage/analytics.jsx
@@ -1,4 +1,93 @@
+// I will be duplicating a lot of code from other branches here but oh well
+
+// Analytics I will start with:
+// Total listings, total reservations, total revenue
+// percentage of listings collected
+// No show rate
+// Average revenue per listing
+// Average revenue per reservation
+
+import useLoadData from '../loadDataHook.jsx';
+
 function Analytics() {
+    const { data: totalListings, loading: loadingListings } = useLoadData('analytics/totalListings', { sellerID: 1 });
+    const { data: totalReservations, loading: loadingReservations } = useLoadData('analytics/totalReservations', { sellerID: 1 });
+    const { data: totalNoShows, loading: loadingNoShows } = useLoadData('analytics/totalNoshows', { sellerID: 1 });
+    const { data: totalRevenue, loading: loadingRevenue } = useLoadData('analytics/totalRevenue', { sellerID: 1 });
+
+    // now define the values for every statistic we will display
+
+    let numListings;
+    let numReservations;
+    let amountRevenue;
+    let percentageListingsCollected;
+    let noShowRate;
+    let avgRevenuePerListing;
+
+
+    // Total number of listings
+    if (loadingListings) {
+        numListings = "Loading..."
+    } else { numListings = totalListings }
+
+    // Total number of reservations
+    if (loadingReservations){
+        numReservations = "Loading..."
+    } else { numReservations = totalReservations}
+
+    // Total Revenue
+    if (loadingRevenue) {
+        amountRevenue = "Loading..."
+    } else { amountRevenue = totalRevenue}
+
+    // percentage of listings collected
+    if (loadingListings || loadingReservations || loadingNoShows) {
+        percentageListingsCollected = "Loading..."
+    } else {
+        let totalUnreserved = totalListings - totalReservations;
+        let totalUncollected = totalUnreserved + totalNoShows;
+        percentageListingsCollected = 100 * (totalUncollected / totalListings)
+    }
+
+    // no show rate
+    if (loadingReservations || loadingNoShows) {
+        noShowRate = "Loading..."
+    } else {
+        noShowRate = 100 * (totalNoShows/totalReservations)
+    }
+
+    // avg revenue per listing
+    if (loadingListings || loadingRevenue) {
+        avgRevenuePerListing = "Loading...";
+    } else {
+        avgRevenuePerListing = 100 * (totalRevenue / totalListings)
+    }
+
+    return (
+        <div>
+            <h2>All time Analytics</h2>
+
+            <h3>Total number of listings posted</h3>
+            <p>{numListings}</p>
+
+            <h3>Total number of listings reserved by a buyer</h3>
+            <p>{numReservations}</p>
+
+            <h3>Total amount of revenue generated</h3>
+            <p>{amountRevenue}</p>
+
+            <h3>Percentage of listings collected</h3>
+            <p>{percentageListingsCollected}%</p>
+
+            <h3>Percentage of noshows for reserved listings</h3>
+            <p>{noShowRate}</p>
+
+            <h3>Average amount of revenue generated per listing</h3>
+            <p>{avgRevenuePerListing}</p>
+        </div>
+    )
+
+
 
 }
 
--- frontend/src/seller/homepage/reservedBundles.jsx
+++ frontend/src/seller/homepage/reservedBundles.jsx
@@ -1,4 +1,16 @@
+import useLoadData from '../loadDataHook.jsx';
+
 function ReservedBundles() {
+
+    const {bundles, loadingBundles} = useLoadData("seller/reservedBundles", {sellerID: 1});
+    // returns a json object containin all bundles reserved, for each bundle:
+    // name of buyer, pickup time, bundle name
+
+
+
+
+
+
 
 }
 
