c387d1b871c0cd7f4c41d419e3e5fd93cf356cca
Harry <your-email@example.com>
2026-02-09 19:53:08 +0000

    Centralised the login and sign up pages by making authLayout and auth.css- this was to make changes to root afffect the pages better. I also changed the font in root to make the website appear more professional

--- frontend/src/reusableComponents/authLayout.jsx
+++ frontend/src/reusableComponents/authLayout.jsx
@@ -3,14 +3,14 @@ import NavBar from "./navBar";
 import "./auth.css";
 
 // wrapper used for login/register pages
-export default function AuthLayout({ title, children }) {
+export default function authLayout({ title, children }) {
   return (
     <div className="auth-page">
       <NavBar />
       <div className="auth-container">
         <div className="auth-card">
           <h1 className="auth-brand">Project-Ate</h1>
-          {/* page title (login / register) */}
+          {/* page title (login / login) */}
           <p className="auth-title">{title}</p>
           {/* form content goes here*/}
           {children}
