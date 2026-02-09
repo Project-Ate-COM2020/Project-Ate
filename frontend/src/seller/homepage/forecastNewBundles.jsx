f6bdba2ce6cc74944d23efa6d278f4e3221dc997
Jamie <17clarkeja@gmail.com>
2026-02-09 21:39:06 +0000

    continued development of UI

--- frontend/src/seller/homepage/forecastNewBundles.jsx
+++ frontend/src/seller/homepage/forecastNewBundles.jsx
@@ -1,5 +1,18 @@
 function ForecastNewBundles() {
 
+    // Biggest bit yet, need to make creation of bundles + forecasting
+
+    /* Bundle Creation
+    Value - Bundle Name
+    Value - Bundle Category
+    value - Contents
+    value - Allergens
+    value - Quantity
+    value - Price
+    value - Pickup Window
+
+    */
+
 }
 
 export default ForecastNewBundles
--- frontend/src/seller/homepage/reservedBundles.jsx
+++ frontend/src/seller/homepage/reservedBundles.jsx
@@ -4,10 +4,29 @@ function ReservedBundles() {
 
     const {bundles, loadingBundles} = useLoadData("seller/reservedBundles", {sellerID: 1});
     // returns a json object containin all bundles reserved, for each bundle:
-    // name of buyer, pickup time, bundle name
+    // name of buyer, pickup time, bundle name, bundle code
+    
+    let render_bundles;
+
+    if (loadingBundles) {
+        render_bundles = <p>Loading...</p>
+    } else {
+        <div>
+            {bundles.map(bundle => (
+                <div key={bundle.id}>
+                    <p>Buyer: {bundle.buyer}</p>
+                    <p>Pickup time: {bundle.time}</p>
+                    <p>Bundle: {bundle.name}</p>
+                    <p>Code: {bundle.code}</p>
+                </div>
+            ))}
+        </div>
+    }
 
 
-
+    return (
+        render_bundles
+    )
 
 
 
