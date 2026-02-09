5fbb5b82310f7d6e91fa665d26564b73b2e444b3
Harry <your-email@example.com>
2026-02-09 18:14:06 +0000

    Changed Nav bar to use nav link and started centralisation for the login/sinup pages

--- frontend/src/reusableComponents/navBar.jsx
+++ frontend/src/reusableComponents/navBar.jsx
@@ -1,22 +1,20 @@
 /* The nav bar goes at the top of the page to help navigate the website
 add in the links to the different webpages needed and badges*/
 
-import './navBar.css';
-
-// future hooks to get streak length and bagdes displayed and well a custom user name 
+import "./navBar.css";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-      <nav>
-        <a href ="/user">Project-Ate</a>
-        <ul>
-            <li><a href="/">Profile</a></li>    
-            {/*<li><a href="/basket">Bakset</a> </li>*/}
-            <li><a href="/">Login</a></li>    
-            <li><a href="/orders">Orders</a></li>     
-            
-        </ul>
-      </nav>
-    );
-  }
-  
+  return (
+    <nav>
+      {/* use NavLink instead of <a href> so react router handles navigation (no page reload) */}
+      <NavLink to="/user">Project-Ate</NavLink>
+      <ul>
+        <li><NavLink to="/">Profile</NavLink></li>
+        {/* <li><NavLink to="/basket">Basket</NavLink></li> */}
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/orders">Orders</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
