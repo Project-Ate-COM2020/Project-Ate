a438170daa4ae3388da6f006ff388d727264b3d4
jacob evans <jacobevans4@proton.me>
2026-02-10 05:54:17 +0000

    implement custom json web token types for seller and consumers

---
+++ backend/marketplace/consumer_token.py
@@ -0,0 +1,23 @@
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    ValidationError,
+)
+from models import Consumer
+from argon2 import PasswordHasher
+
+
+class ConsumerTokenPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        username = attrs["username"]
+        password = attrs["password"]
+
+        ph = PasswordHasher()
+
+        hashed_password = ph.hash(password)
+
+        if not Consumer.objects.filter(
+            username=username, password=hashed_password
+        ).exists():
+            raise ValidationError({"username": "Invalid username and/or password."})
+
+        return super().validate(attrs)
---
+++ backend/marketplace/seller_token.py
@@ -0,0 +1,21 @@
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    ValidationError,
+)
+from models import Seller
+from argon2 import PasswordHasher
+
+
+class SellerTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        ph = PasswordHasher()
+
+        hpassword = ph.hash(password)
+
+        if not (Seller.objects.filter(username=username, password=hpassword).exists()):
+            raise ValidationError({"invalid": "password"})
+
+        return super().validate(attrs)
