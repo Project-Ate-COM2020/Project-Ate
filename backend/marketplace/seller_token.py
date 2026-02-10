d06f1702325295123df85e641142399fc4175004
jacob evans <jacobevans4@proton.me>
2026-02-10 06:23:44 +0000

    fix broken import paths

--- backend/marketplace/seller_token.py
+++ backend/marketplace/seller_token.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.serializers import (
     ValidationError,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
-from models import Seller
+from .models import Seller
 from argon2 import PasswordHasher
 
 
