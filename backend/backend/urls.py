44a03082f4ff108dd440732c294e3109debd7861
willb250111 <will.brown250111@gmail.com>
2026-02-15 22:51:00 +0000

    fixed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("admin/", admin.site.urls),
-    path("api-auth", include("rest_framework.urls")),
-    path("", include("marketplace.urls")),
-=======
+
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     path('game/', include('game.urls')),
