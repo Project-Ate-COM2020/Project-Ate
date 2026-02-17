946f870d905019816a342c63501bb4945c681a79
jacob evans <77414989+Oxyn4@users.noreply.github.com>
2026-02-17 12:41:52 +0000

    add the API enpoint urls to each testing class

--- backend/marketplace/tests.py
+++ backend/marketplace/tests.py
@@ -1,19 +1,31 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from django.urls import reverse
+
+from .models import Seller, Consumer, Reservation, Bundle
+from .views import CreateBundleView, CreateSellerView, CreateReservationView, CreateConsumerView
 
 # Create your tests here.
-class BundlesViewsTests(APITestCase):
+class CreateBundleViewsTests(APITestCase):
     def setUp(self):
+        url = reverse(CreateBundleView.name)
+
         pass
 
-class SellerViewsTests(APITestCase):
+class CreateSellerViewTests(APITestCase):
     def setUp(self):
+        self.url = reverse(CreateSellerView.name)
+
         pass
 
-class ReservationViewsTests(APITestCase):
+class CreateReservationViewsTests(APITestCase):
     def setUp(self):
+        self.url = reverse(CreateReservationView.name)
+
         pass
 
-class ConsumerViewsTests(APITestCase):
+class CreateConsumerViewTests(APITestCase):
     def setUp(self):
+        self.url = reverse(CreateConsumerView.name)
+
         pass
