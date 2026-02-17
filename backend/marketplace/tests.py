ebf6b6e6b3f58234dcf3fa25e94c53fef0b4b3c0
jacob evans <77414989+Oxyn4@users.noreply.github.com>
2026-02-17 12:32:14 +0000

    Added view test classes for testing creating, retrieving, updating and deleting bundles, sellers, reservations and consumers.

--- backend/marketplace/tests.py
+++ backend/marketplace/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class BundlesViewsTests(APITestCase):
+    def setUp(self):
+        pass
+
+class SellerViewsTests(APITestCase):
+    def setUp(self):
+        pass
+
+class ReservationViewsTests(APITestCase):
+    def setUp(self):
+        pass
+
+class ConsumerViewsTests(APITestCase):
+    def setUp(self):
+        pass
