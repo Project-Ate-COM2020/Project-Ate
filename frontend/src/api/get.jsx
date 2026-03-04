5eece5345d29f724b4614716ea464d042b1d2f26
Jamie <17clarkeja@gmail.com>
2026-03-04 14:09:46 +0000

    Created new folder for library - created the get file, implements a get request to the backend, no auth included yet

---
+++ frontend/src/api/get.jsx
@@ -0,0 +1,52 @@
+/* --- Import Statements --- */
+import { useState, useEffect } from 'react';
+
+/* --- File Description --- */
+/* This will be a universal function that will take a path and arguments then return json data, this 
+is being used so only this library needs to interact with logic such as jwt auth tokens and token refreshing,
+meaning all other code can be abstracted making for easier development */
+
+/* --- Main Function Declaration --- */
+function useGetData(endpoint, queryParams = {}) {
+    // define variables with state
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    /* --- Defining the UseEffect Hook --- */
+    useEffect(function() {
+        // Parse query parameters dictionary to create query string
+        // uses proper URI encoding to prevent any invalid queries that could threaten security
+        let queryString = "";
+        if (Object.keys(queryParams).length !== 0) {
+            queryString = "?";
+            for (let param in queryParams) {
+                queryString += encodeURIComponent(param) + "=" + encodeURIComponent(queryParams[param]) + "&";
+            }
+            queryString = queryString.slice(0, -1);
+        }
+
+        // Define async function to fetch data
+        async function fetchData() {
+            try {
+                // Uses HardCoded localhost URL - may need to be changed in production
+                const response = await fetch("http://localhost:8000/" + endpoint + queryString);
+                // convert to JSON 
+                const json = await response.json();
+                setData(json);
+                setLoading(false);
+            // Simple error catching - logs the file to the console
+            } catch (err) {
+                setLoading(false);
+                console.log("ERROR - Could not fetch API Data (check arguments calling get.jsx)");
+            }
+        }
+
+        // Call function
+        fetchData();
+    }, [endpoint, queryParams]);
+
+    return { data, loading };
+}
+
+export default useGetData;
