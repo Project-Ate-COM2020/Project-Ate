679aa167a47a698be92fa8598494121050cf1a2e
Jamie <17clarkeja@gmail.com>
2026-03-04 15:45:27 +0000

    Added Content-Type to header for explicit definition of json content

--- frontend/src/api/get.jsx
+++ frontend/src/api/get.jsx
@@ -6,17 +6,23 @@ import { useState, useEffect } from 'react';
 is being used so only this library needs to interact with logic such as jwt auth tokens and token refreshing,
 meaning all other code can be abstracted making for easier development */
 
+/* --- Current Issues --- */
+// This Hook updates every time the reference changes, to improve performance move to checking value rather than reference
+// Need to parse response for return code - for better error handling
+
 /* --- Main Function Declaration --- */
 function useGetData(endpoint, queryParams = {}) {
     // define variables with state
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
-
     /* --- Defining the UseEffect Hook --- */
     useEffect(function() {
         // Parse query parameters dictionary to create query string
         // uses proper URI encoding to prevent any invalid queries that could threaten security
+
+        setLoading(true);
+
         let queryString = "";
         if (Object.keys(queryParams).length !== 0) {
             queryString = "?";
@@ -30,16 +36,23 @@ function useGetData(endpoint, queryParams = {}) {
         async function fetchData() {
             try {
                 // Uses HardCoded localhost URL - may need to be changed in production
-                // Includes authorisation
+                // Includes authorisation in header format, assumes they are stored in secure local storage
                 const token = localStorage.getItem('access_token');
                 let response = null;
                 if (token != null) {
                     response = await fetch("http://localhost:8000/" + endpoint + queryString, {
-                    headers: { "Authorization" : "Bearer " + token} 
-                });
+                        headers: { 
+                            "Authorization" : "Bearer " + token,
+                            "Content-Type": "application/json"
+                        },
+                    });
                 }
                 else {
-                    response = await fetch("http://localhost:8000/" + endpoint + queryString); 
+                    response = await fetch("http://localhost:8000/" + endpoint + queryString, {
+                        headers: { 
+                            "Content-Type": "application/json"
+                        },
+                    }); 
                 }
                 const json = await response.json();
                 setData(json);
