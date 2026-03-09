c367dfc93891e768a45abc3da04c773e46de79a1
Jamie <17clarkeja@gmail.com>
2026-03-09 15:01:48 +0000

    Coding finished for bundles

--- frontend/src/bundlesComponents/bundles.jsx
+++ frontend/src/bundlesComponents/bundles.jsx
@@ -16,76 +16,82 @@ There will probs be more I need to pass */
 import NavBar from "../reusableComponents/navBar.jsx"
 
 /* --- Test Data Declarations --- */
-const bundleName = "Big Cheese Bundle";
-const bundleCategory = "Dairy";
-const imgPath = "/../../Dairy.jpg";
-const pickupTime = "11:00 - 12:00";
-const seller = "Cheesy Goods Incorporated";
-const buyer = "Bobby";
-const collectionCode = "QWERTY";
-const price = 14.50;
-const location = "Exeter";
+const bundles = [
+    {
+      bundleName: "Big Cheese Bundle",
+      bundleCategory: "Dairy",
+      imgPath: "/Dairy.jpg",
+      pickupTime: "11:00 - 12:00",
+      seller: "Cheesy Goods Incorporated",
+      buyer: "Bobby",
+      collectionCode: "QWERTY",
+      price: 14.5,
+      location: "Exeter",
+    },
+  ];
 
 /* --- Helper Functions  --- */
-function SingleBundle({includedAttributes}) {
+function SingleBundle({bundleData, includedAttributes = []}) {
 
-    let pickupTimeDisplayed;
-    let bundleCategoryDisplayed;
-    let sellerDisplayed;
-    let buyerDisplayed;
-    let collectionCodeDisplayed;
-    let priceDisplayed;
-    let locationDisplayed;
-    let markCollectedButtonDisplayed;
-    let unreserveBundleButton;
+    let pickupTimeDisplayed = false;
+    let bundleCategoryDisplayed = false;
+    let sellerDisplayed = false;
+    let buyerDisplayed = false;
+    let collectionCodeDisplayed = false;
+    let priceDisplayed = false;
+    let locationDisplayed = false;
+    let markCollectedButtonDisplayed = false;
+    let unreserveBundleButton = false;
 
     for (const i of includedAttributes) {
-        if (i == "pickup time") {pickupTimeDisplayed = true;}
-        if (i == "bundle category") {bundleCategoryDisplayed = true;}
-        if (i == "seller") {sellerDisplayed = true;}
-        if (i == "buyer") {buyerDisplayed = true;}
-        if (i == "collection code") {collectionCodeDisplayed = true;}
-        if (i == "price") {priceDisplayed = true;}
-        if (i == "location") {locationDisplayed = true;}
-        if (i == "mark collected button") {markCollectedButtonDisplayed = true;}
-        if (i == "unreserve bundle button") {unreserveBundleButton = true;}
+        if (i === "pickup time") {pickupTimeDisplayed = true;}
+        if (i === "bundle category") {bundleCategoryDisplayed = true;}
+        if (i === "seller") {sellerDisplayed = true;}
+        if (i === "buyer") {buyerDisplayed = true;}
+        if (i === "collection code") {collectionCodeDisplayed = true;}
+        if (i === "price") {priceDisplayed = true;}
+        if (i === "location") {locationDisplayed = true;}
+        if (i === "more info button") {moreInfoButton = true;}
     }
 
     return (
         <div className = "bundleContainer">
-            <img src = {imgPath} />
+            <img src = {bundleData.imgPath} alt = {bundleData.bundleName} />
 
-            <h3>{bundleName}</h3>
+            <h3>{bundleData.bundleName}</h3>
 
-            {pickupTimeDisplayed && <p>pickup time: {pickupTime}</p>}
+            {pickupTimeDisplayed && <p>pickup time: {bundleData.pickupTime}</p>}
 
-            {bundleCategoryDisplayed && <p>Bundle Category: {bundleCategory}</p>}
+            {bundleCategoryDisplayed && <p>Bundle Category: {bundleData.bundleCategory}</p>}
 
-            {sellerDisplayed && <p>Seller: {seller}</p>}
+            {sellerDisplayed && <p>Seller: {bundleData.seller}</p>}
 
-            {buyerDisplayed && <p>Buyer: {buyer}</p>}
+            {buyerDisplayed && <p>Buyer: {bundleData.buyer}</p>}
 
-            {collectionCodeDisplayed && <p>Collection Code: {collectionCode}</p>}
+            {collectionCodeDisplayed && <p>Collection Code: {bundleData.collectionCode}</p>}
 
-            {priceDisplayed && <p>Price: £{price}</p>}
+            {priceDisplayed && <p>Price: £{bundleData.price}</p>}
 
-            {locationDisplayed && <p>Location: {location}</p>}
+            {locationDisplayed && <p>Location: {bundleData.location}</p>}
 
-            {markCollectedButtonDisplayed && <button>Mark Bundle as collected</button>}
-
-            {unreserveBundleButton && <button>Unreserve bundle</button>}
+            {moreInfoButtonDisplayed && <button>More Info</button>}
 
         </div>
     )
 }
 
 /* --- Main Page Function --- */
-function Bundles({includedAttributes=[]}) {
+function Bundles({includedAttributes=[], numberOfBundles = 0, endPoint = "", postBody = {}}) {
     return (
         <div className = "bundlesContainer">
             <NavBar />
-            <SingleBundle includedAttributes/>
-
+            {bundles.slice(0, numberOfBundles).map((bundle) => (
+                <SingleBundle
+                    key={bundle.collectionCode}
+                    bundleData={bundle}
+                    includedAttributes={includedAttributes}
+                />
+            ))}
         </div>
     )
 }
