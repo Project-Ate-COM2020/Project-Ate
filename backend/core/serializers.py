15d2b41c2f61698f2ec4af7be03622328a152057
jacob evans <jacobevans4@proton.me>
2026-03-10 09:30:40 +0000

    add serializers for badge, badge mapping, seller and seller with password to core

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -6,3 +6,36 @@ from backend.core.models import (
     BadgeMapping,
     Badges,
 )
+
+from rest_framework import serializers
+
+
+class BadgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Badges
+        fields = "__all__"
+
+
+class BadgeMappingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BadgeMapping
+        fields = "__all__"
+
+
+class SellerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = ["name", "location", "opening_hours", "contact_stub"]
+
+
+class SellerWithPasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = [
+            "seller_id",
+            "name",
+            "password",
+            "location",
+            "opening_hours",
+            "contact_stub",
+        ]
