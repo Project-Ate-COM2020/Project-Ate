8808b70217653d282e98cfa87666deb58714550b
jacob evans <jacobevans4@proton.me>
2026-03-10 10:22:21 +0000

    fix instances of bundle bieng used as opposed to bundlePosting

--- backend/marketplace/tests.py
+++ backend/marketplace/tests.py
@@ -64,10 +64,10 @@ class CreateBundleViewsTests(APITestCase):
             self.bundle_creation_response.status_code, status.HTTP_201_CREATED
         )
 
-        self.assertEqual(Bundle.objects.count(), 1)
+        self.assertEqual(BundlePosting.objects.count(), 1)
 
     def test_bundle_data_integrity(self):
-        bundle = Bundle.objects.get(id=self.bundle_id)
+        bundle = BundlePosting.objects.get(id=self.bundle_id)
 
         self.assertEqual(bundle.seller.id, self.seller_id)
         self.assertEqual(bundle.category, "food")
