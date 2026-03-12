d9aa5418710c834aba883c66ccbe7ce1f7f3c77f
jacob evans <jacobevans4@proton.me>
2026-03-12 15:02:45 +0000

    add views for creating, retrieving, updating and deleting maintainer database objects

--- backend/maintainer/views.py
+++ backend/maintainer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Maintainer, MaintainerSerializer
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+class CreateMaintainerView(CreateAPIView):
+    name = "maintainer-create"
+    queryset = Maintainer
+    serializer_class = MaintainerSerializer
+
+class MaintainerView(RetrieveUpdateDestroyAPIView):
+    name = "maintainer"
+    queryset = Maintainer.objects.all()
+    serializer_class = MaintainerSerializer
