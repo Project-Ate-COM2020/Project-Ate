8e0381b9fc8c2dd4c95ee82487f8255c0b7c354e
jacob evans <jacobevans4@proton.me>
2026-03-12 19:27:02 +0000

    Created a single User model for authentication, created authentication app for permissions, user account logic. Bound sellers and consumers to new User type. Unlinked old auth system
    # Conflicts:
    #	backend/backend/settings.py
    #	backend/backend/urls.py
    #	backend/core/admin.py
    
    # Conflicts:
    #	backend/authentication/tests.py
    #	backend/authentication/token.py
    #	backend/authentication/urls.py
    #	backend/backend/urls.py
    #	backend/core/migrations/0001_initial.py
    #	backend/marketplace/views/seller.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,16 +53,17 @@ INSTALLED_APPS = [
     # For specific API development
     "rest_framework",
     "rest_framework_simplejwt",
+    
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
