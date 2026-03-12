f1aec7af4b915bcf2b3d6a0950982edddf756eca
jacob evans <jacobevans4@proton.me>
2026-03-12 21:33:11 +0000

    remove old seller and consumer tokens

--- backend/marketplace/views/reservation.py
+++ backend/marketplace/views/reservation.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
