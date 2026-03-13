978a9d4603b88963f291c17697546db94e73bb27
jacob evans <jacobevans4@proton.me>
2026-03-13 14:23:36 +0000

    add a test to test to ensure an ordanairy user cannot access a seller endpoint but a user with a seller property can

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -4,9 +4,13 @@ from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework.test import force_authenticate
 from django.urls import reverse
 
+from .permissions import IsSeller
 from .views import UserCreateView
 
 from marketplace.views import CreateSellerView
+from marketplace.views import CreateBundleView
+
+
 
 class UserTokenTest(APITestCase):
     def setUp(self):
@@ -69,3 +73,82 @@ class UserTokenTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+class TestIsSellerPermission(APITestCase):
+    def setUp(self):
+        create_user_url = reverse(UserCreateView.name)
+
+        self.username = "testuser"
+        self.password = "pass"
+
+        user_data = {
+            "password": self.password,
+            "username": self.username,
+            "email": "testemail@testuser.com",
+            "first_name": "testuser",
+            "last_name": "testuser",
+        }
+
+        self.user = self.client.post(create_user_url, user_data, format="json")
+
+        json = self.user.json()
+
+        self.user_id = json["id"]
+
+        token_url = reverse("user-token")
+
+        data = {
+            "username": self.username,
+            "password": self.password,
+        }
+
+        response = self.client.post(token_url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.token_obtain_response = response.json()
+
+
+    def test_user_cannot_access(self):
+        url = reverse(CreateBundleView.name)
+
+        response = self.client.post(url, {}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_seller_protected_viewpoint(self):
+        # create bundle is a seller protected view
+
+        create_seller_url = reverse(CreateSellerView.name)
+
+        data = {
+            "name": "seller",
+            "contact_stub": "53636",
+            "user_id": self.user_id,
+            "location": "exeter",
+        }
+
+        authorization_string = f"Bearer {self.token_obtain_response["access"]}"
+
+        self.authorization_headers = {"AUTHORIZATION": authorization_string}
+
+        seller_creation_response = self.client.post(create_seller_url, data, format="json", headers=self.authorization_headers)
+
+        self.assertEqual(seller_creation_response.status_code, status.HTTP_201_CREATED)
+
+        # refresh the token to clarify we are a seller
+        url = reverse("token-refresh")
+
+        refresh = self.client.post(url, {"refresh": self.token_obtain_response["refresh"]}, format="json")
+
+        # update access token
+        self.token_obtain_response["access"] = refresh.json()["access"]
+
+        url = reverse(CreateBundleView.name)
+
+        authorization_string = f"Bearer {self.token_obtain_response["access"]}"
+
+        self.authorization_headers = {"AUTHORIZATION": authorization_string}
+
+        response = self.client.post(url, {}, format="json", headers=self.authorization_headers)
+
+        self.assertNotEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
