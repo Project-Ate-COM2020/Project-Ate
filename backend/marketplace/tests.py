742822dd9cd94750f4b222a1d321b3d35fc1c82f
jacob evans <jacobevans4@proton.me>
2026-03-16 19:47:08 +0000

    add error checking to creation serializers

--- backend/marketplace/tests.py
+++ backend/marketplace/tests.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
-from authentication.tests import setup_random_seller
+from authentication.tests import setup_random_seller, setup_random_user
 
 from authentication.token import UserTokenObtainPairSerializer
 
@@ -113,37 +113,42 @@ class CreateBundleViewsTests(APITestCase):
 
 class CreateSellerViewTests(APITestCase):
     def setUp(self):
-        url = reverse(CreateSellerView.name)
+        self.url = reverse(CreateSellerView.name)
 
         self.user, self.headers = create_user_get_auth_headers(
             "test", "test@test.com", "password"
         )
 
-        seller_data = {
+        self.seller_data = {
             "name": "lauren",
             "location": "CF54BB",
             "opening_hours": "00:00-24:00",
             "contact_stub": "9874325655",
         }
 
+    def post_create_seller(self):
         self.creation_response = self.client.post(
-            url, seller_data, format="json", headers=self.headers
+            self.url, self.seller_data, format="json", headers=self.headers
         )
 
-        self.seller_id = self.creation_response.json()["seller_id"]
+        return self.creation_response.json()["seller_id"]
 
     def test_creation_success(self):
+        self.post_create_seller()
+
         self.assertEqual(Seller.objects.count(), 1)
 
         self.assertEqual(self.creation_response.status_code, status.HTTP_201_CREATED)
 
     def test_data_integrity(self):
-        seller = Seller.objects.get(seller_id=self.seller_id)
+        seller_id = self.post_create_seller()
 
-        self.assertEqual(seller.name, "lauren")
-        self.assertEqual(seller.location, "CF54BB")
-        self.assertEqual(seller.opening_hours, "00:00-24:00")
-        self.assertEqual(seller.contact_stub, "9874325655")
+        seller = Seller.objects.get(seller_id=seller_id)
+
+        self.assertEqual(seller.name, self.seller_data["name"])
+        self.assertEqual(seller.location, self.seller_data["location"])
+        self.assertEqual(seller.opening_hours, self.seller_data["opening_hours"])
+        self.assertEqual(seller.contact_stub, self.seller_data["contact_stub"])
 
 
 class CreateReservationViewsTests(APITestCase):
@@ -194,7 +199,6 @@ class CreateReservationViewsTests(APITestCase):
 
         reservation_data = {
             "posting": int(self.bundle_id),
-            "consumer": int(self.consumer_id),
             "claim_code": "XXXXXX",
             "status": "collected",
         }
