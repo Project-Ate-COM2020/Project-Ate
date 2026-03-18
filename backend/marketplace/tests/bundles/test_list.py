78e489fb99c2516b8bb5c2ace5c76df150e133e5
jacob evans <jacobevans4@proton.me>
2026-03-18 22:31:49 +0000

    write list bundles view test

--- backend/marketplace/tests/bundles/test_list.py
+++ backend/marketplace/tests/bundles/test_list.py
@@ -1,6 +1,14 @@
+from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
+from authentication.tests import (
+    setup_random_reservation,
+    setup_random_bundle,
+    setup_random_bundle_for_seller,
+    setup_random_consumer,
+    get_authorization_headers_for_user,
+)
 from marketplace.views import ListBundlesView
 
 
@@ -9,4 +17,21 @@ class ListBundleViewsTests(APITestCase):
         self.url = reverse(ListBundlesView.name)
 
     def test_user_can_see_bundles(self):
-        pass
+        cuser, consumer = setup_random_consumer()
+
+        headers = get_authorization_headers_for_user(cuser)
+
+        suser, seller, bundle = setup_random_bundle()
+
+        for x in range(5):
+            _ = setup_random_bundle_for_seller(seller)
+
+        response = self.client.get(
+            self.url, {"page": 0}, format="json", headers=headers
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = response.json()
+
+        self.assertEqual(len(response), 6)
