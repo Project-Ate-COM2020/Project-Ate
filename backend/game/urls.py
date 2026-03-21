c29a6dc615d268a53e0dc4c9ee117aa8b9cb3247
jacob evans <jacobevans4@proton.me>
2026-03-21 05:17:51 +0000

    add summary view back in

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GameSummaryView, RecentRescuesView, ConsumerBadgesView
 
 urlpatterns = [
-    path("summary/", GameSummaryView.as_view(), name="game-summary"),
-    path("recent/", RecentRescuesView.as_view(), name="game-recent"),
+    path("summary/", GameSummaryView.as_view(), name=GameSummaryView.name),
+    path("recent/", RecentRescuesView.as_view(), name=RecentRescuesView.name),
     path("badges/", ConsumerBadgesView.as_view(), name=ConsumerBadgesView.name),
 ]
