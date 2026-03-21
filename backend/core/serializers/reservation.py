9047a0a1f998d567640628f17e3aa26dfca6745b
jacob evans <jacobevans4@proton.me>
2026-03-21 05:00:23 +0000

    add migration to add badges to db. Add tests for the get badges view

--- backend/core/serializers/reservation.py
+++ backend/core/serializers/reservation.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.db.models.aggregates import Count
+from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -63,7 +66,9 @@ class CreateReservationSerializer(serializers.ModelSerializer):
         return reservation
 
 
-from game.constants import CO2_PER_ITEM
+from game.constants import get_co2_per_item
+
+CO2_PER_ITEM = get_co2_per_item()
 
 
 class ConsumerUpdateReservationSerializer(serializers.ModelSerializer):
@@ -86,6 +91,7 @@ class SellerUpdateReservationSerializer(serializers.ModelSerializer):
         match new_status:
             case "collected":
                 instance.status = "collected"
+                instance.collected_at = timezone.now()
 
                 instance.save()
 
@@ -113,17 +119,12 @@ class SellerUpdateReservationSerializer(serializers.ModelSerializer):
                     min_categories__lte=consumer.categories_collected,
                     min_co2__lte=consumer.co2_saved,
                 )
-                # print("can have ", badges_can_have)
 
                 already_have = Badges.objects.filter(
                     consumers_who_have_earned__consumer_id=consumer,
                 )
 
-                # print("already have", already_have)
-
                 badges_to_add = badges_can_have.difference(already_have)
-
-                # print("badges to add:", badges_to_add)
 
                 join = [
                     BadgeMapping(consumer_id=consumer, badge_id=b)
@@ -131,6 +132,26 @@ class SellerUpdateReservationSerializer(serializers.ModelSerializer):
                 ]
 
                 BadgeMapping.objects.bulk_create(join)
+
+                last_collected = (
+                    Reservation.objects.filter(status="collected")
+                    .order_by("-collected_at")
+                    .first()
+                )
+
+                if last_collected is not None:
+                    # check
+                    timestamp = last_collected.collected_at
+                    now = instance.collected_at
+
+                    satisfies_streak = now.date() == (
+                        timestamp.date() + timedelta(days=1)
+                    )
+
+                    if satisfies_streak:
+                        consumer.streak += 1
+
+                consumer.save()
 
                 return instance
             case "no-show":
