57dfd9dff1b05a1853f21b6f2232bc071eb3a48d
Jamie <17clarkeja@gmail.com>
2026-03-22 17:58:17 +0000

    Development should now be updated w my branch

--- backend/game/constants.py
+++ backend/game/constants.py
@@ -50,3 +50,27 @@ def get_co2_per_item():
             "desserts": 1.0,
             "dairy": 1.5,
         }
+
+CO2_PER_ITEM = {
+    "hot_meals": 2.5,
+    "fresh_produce": 0.5,
+    "prepared_salads": 1.2,
+    "bakery": 0.8,
+    "desserts": 1.0,
+    "dairy": 1.5,
+}
+
+VARIETY_BADGES = [
+    {"name": "Explorer", "min_categories": 2, "min_co2": 0},
+    {"name": "Discoverer", "min_categories": 3, "min_co2": 0},
+    {"name": "Adventurer", "min_categories": 4, "min_co2": 0},
+    {"name": "Master", "min_categories": 6, "min_co2": 0},
+]
+
+# took a guess here with the co2 badges, might need a revisit
+IMPACT_BADGES = [
+    {"name": "Eco Starter", "min_co2": 100, "min_categories": 0},
+    {"name": "Eco Friend", "min_co2": 500, "min_categories": 0},
+    {"name": "Climate Hero", "min_co2": 1000, "min_categories": 0},
+    {"name": "Planet Saver", "min_co2": 10000, "min_categories": 0},
+]
